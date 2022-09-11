638131d85e7ed33dcec927cfe2e6db8f93074b34
varshasarawat18 <varshasarawat18@gmail.com>
2022-09-11 16:30:36 +0530

    MavenSelenium

--- src/test/java/TestCases/SimpliLearnLoginTest.java
+++ src/test/java/TestCases/SimpliLearnLoginTest.java
@@ -1,12 +1,10 @@
 package TestCases;
 
-import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-
 import Pages.LoginPage;
 
 public class SimpliLearnLoginTest extends BaseClass {
